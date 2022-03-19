80aa6fed8e3269ff1d395673cd8bad10b750a4db
Ahmet Oğuz Koca <aoguzkoca@hotmail.com>
2022-03-20 00:15:25 +0300

    Eğitimdeki diğer eksik öğeler eklendi.

---
+++ MauiEgitim/Models/WeatherForecast.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiEgitim.Models
+{
+
+    public class WeatherForecast
+    {
+        public WeatherForecast[] Weathers { get; set; }
+    }
+
+    public class Weather
+    {
+        public DateTime date { get; set; }
+        public int temperatureC { get; set; }
+        public int temperatureF { get; set; }
+        public string summary { get; set; }
+    }
+
+  
+}
--- MauiEgitim/wwwroot/js/custom.js
+++ MauiEgitim/wwwroot/js/custom.js
@@ -8,8 +8,6 @@ function MyFunc() {
     alert('hello');
 }
 
-
-
 //call dotnet code from javascript
 
 
