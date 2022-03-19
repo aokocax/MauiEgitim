6fad54b014bd6235730e7139ee9049659bec1faf
Ahmet Oğuz Koca <aoguzkoca@hotmail.com>
2022-03-19 17:19:25 +0300

    Canera video record

---
+++ MauiEgitim/Pages/ShowCamera.razor.cs
@@ -0,0 +1,28 @@
+﻿using Android.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiEgitim.Pages
+{
+    public partial class ShowCamera
+    {
+		private async Task<PermissionStatus> GetPermission()
+		{
+			var status = Permissions.RequestAsync<Permissions.Camera>();
+			if (status.Result != PermissionStatus.Granted)
+			{
+				await Application.Current.MainPage.DisplayAlert("Yetki", "Yetki sağlanamadı", "OK");
+
+			}
+			return await status;
+		}
+		async void ShowIt()
+		{
+
+			var result = await MediaPicker.CaptureVideoAsync(); 
+		}
+	}
+}
