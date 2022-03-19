e24dfeaca93fc60e0225d83f1574e405d46627f7
Ahmet Oğuz Koca <aoguzkoca@hotmail.com>
2022-03-19 16:59:59 +0300

    Permission and Routing added

--- MauiEgitim/Platforms/Android/MainActivity.cs
+++ MauiEgitim/Platforms/Android/MainActivity.cs
@@ -1,10 +1,28 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Plugin.CurrentActivity;
 
 namespace MauiEgitim;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        CrossCurrentActivity.Current.Init(this, savedInstanceState);
+
+        Microsoft.Maui.Essentials.Platform.Init(this, savedInstanceState); // add this line to your code, it may also be called: bundle
+
+
+    }
+
+    public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+    {
+        Microsoft.Maui.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 }
